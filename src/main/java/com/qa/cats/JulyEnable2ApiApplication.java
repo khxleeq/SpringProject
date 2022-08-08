641a503d4fee0f8c40f40fa3bef351c1ea7d29d4
Khaleeq <khaleeq.tanweer@gmail.com>
2022-08-08 14:07:31 +0100

    Update

--- src/main/java/com/qa/cats/JulyEnable2ApiApplication.java
+++ src/main/java/com/qa/cats/JulyEnable2ApiApplication.java
@@ -2,6 +2,8 @@ package com.qa.cats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class JulyEnable2ApiApplication {
@@ -11,3 +13,4 @@ public class JulyEnable2ApiApplication {
 	}
 
 }
+
---
+++ src/main/java/com/qa/cats/rest/CatController.java
@@ -0,0 +1,15 @@
+package com.qa.cats.rest;
+
+
+import com.qa.cats.domain.Cat;
+import org.springframework.web.bind.annotation.RestController;
+import com.qa.cats.domain.Cat;
+import java.util.List;
+
+@RestController
+public class CatController {
+
+    private List<Cat> cats;
+
+
+}
