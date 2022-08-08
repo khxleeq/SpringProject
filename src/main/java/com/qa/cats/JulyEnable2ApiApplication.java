04d3138c93e49542f080a8083917c93fe233903c
Khaleeq <khaleeq.tanweer@gmail.com>
2022-08-08 11:24:15 +0100

    Configured Maven project and added dependencies

---
+++ src/main/java/com/qa/cats/JulyEnable2ApiApplication.java
@@ -0,0 +1,13 @@
+package com.qa.cats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulyEnable2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulyEnable2ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/cats/JulyEnable2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.cats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulyEnable2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
