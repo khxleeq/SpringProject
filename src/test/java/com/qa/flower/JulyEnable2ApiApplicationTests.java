c4b618af9cee9af167adfc5522f75b555a06f2b0
Khaleeq <khaleeq.tanweer@gmail.com>
2022-08-10 12:27:08 +0100

    Update

--- src/test/java/com/qa/flower/JulyEnable2ApiApplicationTests.java
+++ src/test/java/com/qa/flower/JulyEnable2ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.qa.cats;
+package com.qa.flower;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ src/test/java/com/qa/flower/rest/FlowerControllerIntegrationTest.java
@@ -0,0 +1,57 @@
+package com.qa.flower.rest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.flower.domain.Flower;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FlowerControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+
+    @Test
+    void testCreate() throws Exception {
+        Flower testFlower = new Flower("Rose","Red", "Sweet");
+        RequestBuilder req = post("/createFlower").content(this.mapper.writeValueAsString(testFlower))
+                .contentType(MediaType.APPLICATION_JSON);
+
+        ResultMatcher checkStatus = status().isCreated();
+        Flower testSavedFlower = new Flower("Rose", "Red", "Sweet");
+        ResultMatcher checkBody = content().json(this.mapper.writeValueAsString(testSavedFlower));
+
+
+        this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+
+
+
+
+
+    }
+
+    @Test
+    void testCreateButHardToRead() throws Exception {
+        this.mvc.perform(post("/createFlower").content(this.mapper.writeValueAsString(new Flower("Rose", "Red", "Sweet")))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(this.mapper.writeValueAsString(new Flower("Rose", "Red", "Sweet"))));
+    }
+
+
+}
