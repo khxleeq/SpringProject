40d5dd7141aad730daf0c97195d433bb0cc91e04
Khaleeq <khaleeq.tanweer@gmail.com>
2022-08-11 12:33:00 +0100

    + active profiles

--- src/test/java/com/qa/flower/JulyEnable2ApiApplicationTests.java
+++ src/test/java/com/qa/flower/JulyEnable2ApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.qa.flower;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JulyEnable2ApiApplicationTests {
 
 	@Test
--- src/test/java/com/qa/flower/rest/FlowerControllerIntegrationTest.java
+++ src/test/java/com/qa/flower/rest/FlowerControllerIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -22,6 +23,7 @@ import java.util.List;
 @AutoConfigureMockMvc
 @Sql(scripts = { "classpath:flower-schema.sql",
         "classpath:flower-data.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test") // set Profile to "test" so it uses the test app-test.properties
 public class FlowerControllerIntegrationTest {
 
     @Autowired
